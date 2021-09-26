30576b1b8e08a6a15dea697d0537f0a5a8b48359
Roman Yeremenko <46825200+roalyr@users.noreply.github.com>
2021-09-26 15:40:50 +0300

    Exceptions list and blacklist moved in the beginning, any exceptions found will be printed out but not touched at all.

--- sanitize_filenames_recursively_here.py
+++ sanitize_filenames_recursively_here.py
@@ -1,3 +1,22 @@
+# Blacklisted symbols that will be replaced in filenames.
+blacklist = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0"]
+	
+# Reserved words on Windows (and other exceptions).
+reserved = [
+	"CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5",
+	"COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5",
+	"LPT6", "LPT7", "LPT8", "LPT9", "Thumbs.db:encryptable", 
+]
+
+"""
+Return a fairly safe version of the filename.
+We don't limit ourselves to ascii, because we want to keep municipality
+names, etc, but we do want to get rid of anything potentially harmful,
+and make sure we do not exceed Windows filename length limits.
+Hence a less safe blacklist, rather than a whitelist.
+"""
+
+
 # Below is the section of code taken and re-used with original licence from:
 # https://pypi.org/project/sanitize-filename/ by Leo Wallentin | J++ Stockholm
 
@@ -29,94 +48,83 @@
 import unicodedata
 import re
 
+# A list to fill up with exceptions for manual treatment.
+exceptions = []
 
-def sanitize(filename):
-	"""Return a fairly safe version of the filename.
-
-	We don't limit ourselves to ascii, because we want to keep municipality
-	names, etc, but we do want to get rid of anything potentially harmful,
-	and make sure we do not exceed Windows filename length limits.
-	Hence a less safe blacklist, rather than a whitelist.
-	"""
-	blacklist = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0"]
-	reserved = [
-		"CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5",
-		"COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5",
-		"LPT6", "LPT7", "LPT8", "LPT9",
-	]  # Reserved words on Windows
-	filename = "".join(c for c in filename if c not in blacklist)
-	# Remove all charcters below code point 32
-	filename = "".join(c for c in filename if 31 < ord(c))
-	filename = unicodedata.normalize("NFKD", filename)
-	filename = filename.rstrip(". ")  # Windows does not allow these at end
-	filename = filename.strip()
-	if all([x == "." for x in filename]):
-		filename = "__" + filename
+def sanitize(filename, root):
 	if filename in reserved:
-		filename = "__" + filename
-	if len(filename) == 0:
-		filename = "__"
-	if len(filename) > 255:
-		parts = re.split(r"/|\\", filename)[-1].split(".")
-		if len(parts) > 1:
-			ext = "." + parts.pop()
-			filename = filename[:-len(ext)]
-		else:
-			ext = ""
-		if filename == "":
-			filename = "__"
-		if len(ext) > 254:
-			ext = ext[254:]
-		maxl = 255 - len(ext)
-		filename = filename[:maxl]
-		filename = filename + ext
-		# Re-check last character (if there was no extension)
-		filename = filename.rstrip(". ")
+		exceptions.append(os.path.join(root, filename))
+	else:
+		filename = "".join(c for c in filename if c not in blacklist)
+		# Remove all charcters below code point 32
+		filename = "".join(c for c in filename if 31 < ord(c))
+		filename = unicodedata.normalize("NFKD", filename)
+		filename = filename.rstrip(". ")  # Windows does not allow these at end
+		filename = filename.strip()
+		if all([x == "." for x in filename]):
+			filename = "__" + filename
 		if len(filename) == 0:
 			filename = "__"
+		if len(filename) > 255:
+			parts = re.split(r"/|\\", filename)[-1].split(".")
+			if len(parts) > 1:
+				ext = "." + parts.pop()
+				filename = filename[:-len(ext)]
+			else:
+				ext = ""
+			if filename == "":
+				filename = "__"
+			if len(ext) > 254:
+				ext = ext[254:]
+			maxl = 255 - len(ext)
+			filename = filename[:maxl]
+			filename = filename + ext
+			# Re-check last character (if there was no extension)
+			filename = filename.rstrip(". ")
+			if len(filename) == 0:
+				filename = "__"
 	return filename
 	
 
 def test_invalid_chars():
-    """Make sure invalid characters are removed in filenames."""
-    assert(sanitize("A/B/C") == "ABC")
-    assert(sanitize("A*C.d") == "AC.d")
+	"""Make sure invalid characters are removed in filenames."""
+	root = "/abc/efg"
+	assert(sanitize("A/B/C",root) == "ABC")
+	assert(sanitize("A*C.d",root) == "AC.d")
 
 
 def test_invalid_suffix():
-    """Dots are not allowed at the end."""
-    assert(sanitize("def.") == "def")
-    assert(sanitize("def.ghi") == "def.ghi")
-    assert(sanitize("X" * 1000 + ".").endswith("X"))
-
-
-def test_reserved_words():
-    """Make sure reserved Windows words are prefixed."""
-    assert(sanitize("NUL") == "__NUL")
-    assert(sanitize("..") == "__")
+	"""Dots are not allowed at the end."""
+	root = "/abc/efg"
+	assert(sanitize("def.",root) == "def")
+	assert(sanitize("def.ghi",root) == "def.ghi")
+	assert(sanitize("X" * 1000 + ".",root).endswith("X"))
 
 
 def test_long_names():
-    """Make sure long names are truncated."""
-    assert(len(sanitize("X" * 300)) == 255)
-    assert(len(sanitize(".".join(["X" * 100, "X" * 100, "X" * 100]))) == 255)
-    assert(len(sanitize(".".join(["X" * 300, "X" * 300, "X" * 300]))) == 255)
-    assert(len(sanitize("." * 300 + ".txt")) == 255)
+	"""Make sure long names are truncated."""
+	root = "/abc/efg"
+	assert(len(sanitize("X" * 300, root)) == 255)
+	assert(len(sanitize(".".join(["X" * 100, "X" * 100, "X" * 100]),root)) == 255)
+	assert(len(sanitize(".".join(["X" * 300, "X" * 300, "X" * 300]),root)) == 255)
+	assert(len(sanitize("." * 300 + ".txt", root)) == 255)
 
 
 def test_unicode_normalization():
-    """Names should be NFKD normalized."""
-    assert(sanitize("ў") == chr(1091)+chr(774))
+	"""Names should be NFKD normalized."""
+	root = "/abc/efg"
+	assert(sanitize("ў", root) == chr(1091)+chr(774))
 
 
 def test_extensions():
-    """Filename extensions should be preserved when possible."""
-    really_long_name = "X" * 1000 + ".pdf"
-    assert(sanitize(really_long_name).endswith(".pdf"))
-    assert(sanitize("X" * 1000).endswith("X"))
-    assert(sanitize("X" * 100 + "." + "X" * 100 + ".pdf").endswith(".pdf"))
-    assert(sanitize("X" * 100 + "." + "X" * 400).endswith("X"))
-    assert(sanitize("X" * 100 + "." + "X" * 400 + ".pdf").endswith(".pdf"))
+	"""Filename extensions should be preserved when possible."""
+	really_long_name = "X" * 1000 + ".pdf"
+	root = "/abc/efg"
+	assert(sanitize(really_long_name, root).endswith(".pdf"))
+	assert(sanitize("X" * 1000, root).endswith("X"))
+	assert(sanitize("X" * 100 + "." + "X" * 100 + ".pdf", root).endswith(".pdf"))
+	assert(sanitize("X" * 100 + "." + "X" * 400, root).endswith("X"))
+	assert(sanitize("X" * 100 + "." + "X" * 400 + ".pdf", root).endswith(".pdf"))
 	
 # ==============================================================================
 # End of the borrowed section of code.
@@ -146,7 +154,6 @@ test_extensions()
 test_invalid_chars()
 test_invalid_suffix()
 test_long_names()
-test_reserved_words()
 test_unicode_normalization()
 
 def c_prompt(prompt): 
@@ -155,12 +162,24 @@ def c_prompt(prompt):
 	return inp 
 
 def cl(): os.system('cls' if os.name == 'nt' else 'clear')
+
+def divider(): 
+	d_line = '─' * 80
+	print(d_line)
+
+def get_exceptions():
+	divider()
+	print(col.WARNING, "Filenames were not changed due to being exceptions (see reserved list).", col.ENDC)
+	divider()
+	for filename in exceptions:
+		print(col.WARNING, filename, col.ENDC)
+	print()
 	
 def sanitize_recursively():
 	print()
 	for root, subdirs, files in os.walk("."):
 		for entry in files:
-			sanitized = sanitize(entry)
+			sanitized = sanitize(entry, root)
 			if entry != sanitized:
 				print(root)
 				print("inp:", col.RED, entry, col.ENDC)
@@ -177,18 +196,21 @@ def sanitize_recursively():
 						print(col.RED, "File name was not changed", col.ENDC)
 					else:
 						raise  # re-raise previously caught exception
+	get_exceptions()
 
+				
 				
 def sanitize_recursively_cold():
 	print()
 	for root, subdirs, files in os.walk("."):
 		for entry in files:
-			sanitized = sanitize(entry)
+			sanitized = sanitize(entry, root)
 			if entry != sanitized:
 				print(root)
 				print("inp (COLD RUN):", col.RED, entry, col.ENDC)
 				print("out (COLD RUN):", col.OKGREEN, sanitized, col.ENDC)
 				print()
+	get_exceptions()
 		
 if __name__ == "__main__":
 	cl()
@@ -202,7 +224,3 @@ if __name__ == "__main__":
 		sanitize_recursively()
 	elif (inp == "C") or (inp == "c"):
 		sanitize_recursively_cold()
-
-
-
-
